8608bca10177231d1c4432bb484ada37205c8f3e
NatalijaZ <nzhilkevich@yandex.ru>
2018-05-07 04:16:17 +0400

    release1

---
+++ Documents/NetBeansProjects/Task1/src/Task1.java
@@ -0,0 +1,50 @@
+/*
+ * Составить программу для вычисления значений функции F(x) на отрезке [а, b] с шагом h. 
+ * Размеры отрезка и параметры задаются в качестве параметров в консоли.
+ * Результат представить в виде таблицы, первый столбец которой – значения аргумента, 
+ * второй - соответствующие значения функции:
+ * F(x) = tg(2x) - 3
+ */
+
+/**
+ * @author N. Zhilkevich
+ * group 23531/21
+ */
+import java.util.Scanner;//импорт класса Scanner для ввода чисел
+
+public class Task1 {
+    public static void main(String[] args) {
+    double a,b,h,x;
+    
+    Scanner scan = new Scanner(System.in);//создание объекта класса Scanner для ввода чисел а,b,h
+    
+    System.out.print("Введите левую границу отрезка (а):");
+    a = scan.nextDouble();
+    //scan.close();
+    
+    System.out.print("Введите правую границу отрезка (b):");
+    b = scan.nextDouble();
+    //scan.close(); 
+    
+    System.out.print("Введите шаг (h):");
+    h = scan.nextDouble();
+    scan.close();
+    
+    if (a>=b){
+        System.out.println("Число \"a\" должно быть меньше числа \"b\"");
+        }
+    else if (h>=b-a){
+        System.out.println("Число \"h\" должно быть больше или равно \"b-a\"");
+    }
+    else {
+        int n;
+            for (x=a;x<=b;x=x+h){
+                for (n=1; (2*x)!=(3.14 / 2) + (3.14 * n++);){//проверка на область определения функции tg
+                    double FunctionValue= Math.tan(2*x) - 3;
+                    System.out.println(x + "   |   " + FunctionValue + "\n");
+                    break;//x=x+h;
+                }
+            }
+    }
+    }  
+}
